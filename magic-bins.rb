d92c293322d5837348d2289584eef747e9c228a0
João Vítor Carli <jvitor.carli@tutamail.com>
2021-05-21 21:08:57 -0300

    [BUGFIX] Fix magic-bins.rb urls. Url must be a zip or a tarball file

--- magic-bins.rb
+++ magic-bins.rb
@@ -1,25 +1,22 @@
 class MagicBins < Formula
-  desc "Magic (Bin)arie(s) for everyday use."
-  homepage "https://github.com/jvcarli/magic-bins/"
-  url "https://github.com/jvcarli/magic-bins.git"
-  version "0.1"
-  sha256 ""
-  license "MIT"
+  desc "Magic (Bin)arie(s) for everyday use"
+  homepage "https://github.com/jvcarli/magic-bins"
+  url "https://github.com/jvcarli/magic-bins/archive/refs/tags/0.1.tar.gz"
+  sha256 "de7082c089705d8281da10d326ef61e75b468b0395d876dd9970344ace7d99da"
+  license "GPL-3.0-only"
 
   depends_on "buku"
   depends_on "fzf"
   depends_on "tmux"
 
   def install
-
     # See: https://stackoverflow.com/questions/46478540/how-to-create-homebrew-formula-with-only-scripts
-    # move *.sh under /usr/local/bin/magic-bins/*
-    bin.install Dir["magic-bins/*.sh"]
-
+    # remove .sh extension from script
+    bin.install "bksrx.sh" => "bksrx"
   end
 
   test do
-    # Turn unnecesary off tests
+    # Turn off unnecessary tests
     system "false"
   end
 end
